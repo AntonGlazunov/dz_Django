966f0f1d9e7de9ce60d2ae3da64b4680a1a7b7d2
Anton Glazunov <tagl91@yandex.ru>
2024-06-24 01:43:25 +0300

    add admin

--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование')
     description = models.TextField(verbose_name='описание')
