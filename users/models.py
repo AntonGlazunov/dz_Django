101d84b441ada8e077e2867c6278d65dc0c297b1
Anton Glazunov <tagl91@yandex.ru>
2024-08-09 18:52:28 +0300

    add login/logout and register

--- users/models.py
+++ users/models.py
@@ -7,7 +7,6 @@ from catalog.models import NULLABLE
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='почта')
-
     avatar = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     phone = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     country = models.CharField(max_length=100, verbose_name='страна', **NULLABLE)
